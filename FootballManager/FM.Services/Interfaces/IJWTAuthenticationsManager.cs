a9c18d5576db899b6da58d2152d514a856b0ee15
Kristiyan Ivanov <kristianiva88@gmail.com>
2025-03-09 14:44:12 +0200

    sln setup

---
+++ FootballManager/FM.Services/Interfaces/IJWTAuthenticationsManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM.Services.Interfaces
+{
+    public interface IJWTAuthenticationsManager
+    {
+        string? Authenticate(string clientId, string secret);
+    }
+}
